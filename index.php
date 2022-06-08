8d5d8a70cc478c1c2807b3227060b80762dea84f
mustafaabuelmagd <37253065+mustafa-abuelmagd@users.noreply.github.com>
2022-06-08 20:32:30 +0200

    fdsa

--- index.php
+++ index.php
@@ -20,10 +20,7 @@ $router->post('/addProduct', Products::class . '::add_new_product');
 
 $router->delete('/deleteProducts', Products::class . '::delete_products');
 
-//
-//$router->delete('/deleteProduct', function () {
-//    require_once DirName . '/api/posts/delete_product.php';
-//});
+
 
 $router->get('/getAllProductTypes', Products::class . '::get_all_product_types');
 
