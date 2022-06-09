af6e04bd16241e32aae8d7633b536a1e197013b2
mustafaabuelmagd <37253065+mustafa-abuelmagd@users.noreply.github.com>
2022-06-09 13:42:05 +0200

    dsf

--- index.php
+++ index.php
@@ -8,24 +8,23 @@ define('DirName', getcwd());
 
 $router = new Router();
 
+// BASIC REQUIRED OPERATIONS
 
 $router->get('/getAllProducts', Products::class . '::get_all_products');
-$router->get('/getProduct', Products::class . '::get_product');
-
 
 $router->post('/addProduct', Products::class . '::add_new_product');
 
-
-
-
 $router->delete('/deleteProducts', Products::class . '::delete_products');
 
+//$router->get('/getProduct', Products::class . '::get_product');
 
+
+
+// ADDITIONAL OPERATIONS FOR SCALABILITY
 
 $router->get('/getAllProductTypes', Products::class . '::get_all_product_types');
 
 $router->post('/addProductType', Products::class . '::add_new_product_type');
-
 
 $router->get('/getApplicationData', Products::class . '::get_types_data');
 
@@ -33,11 +32,9 @@ $router->get('/getAllProductProperties', Products::class . '::get_all_product_pr
 
 $router->post('/addProductProperty', Products::class . '::add_product_property');
 
-
 $router->get('/getAllTypeProperties', Products::class . '::get_all_type_properties');
 
 $router->post('/addTypeProperty', Products::class . '::add_type_property');
-
 
 $router->addNotFoundHandler('/', function () {
     echo 'Home lol';
